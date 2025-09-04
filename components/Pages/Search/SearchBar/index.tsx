83f8f4ae20282ff68442e4944747e57e36a00eb8
Tim <t27.jones@connect.qut.edu.au>
2025-09-04 19:00:37 +1000

    Add mobile styles to Search page

--- components/Pages/Search/SearchBar/index.tsx
+++ components/Pages/Search/SearchBar/index.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames'
 import { FC } from 'react'
 import Container from '~/components/Layout/Container'
 import useBreakpoints from '~/hooks/useBreakpoints'
@@ -13,7 +14,7 @@ const SearchBar: FC<SearchBarProps> = ({ hasResults, searchQuery, setSearchQuery
 
   return (
     <section className="bg-black">
-      <Container className="flex flex-col justify-center pt-20 pb-10 md:items-center">
+      <Container className="flex flex-col justify-center pt-12 pb-10 md:items-center md:pt-20">
         {/* Products */}
         <input
           id="search"
@@ -21,11 +22,16 @@ const SearchBar: FC<SearchBarProps> = ({ hasResults, searchQuery, setSearchQuery
           type="search"
           placeholder={isMobile ? 'Search products...' : 'What are you looking for?'}
           onChange={(e) => setSearchQuery(e.target.value)}
-          className="body-large mx-auto w-full max-w-[1200px] border-b-4 border-white pb-4 text-white placeholder:text-white/50 md:pb-10 md:text-center md:text-[48px] md:leading-[58px] md:-tracking-[1.44px]"
+          className="mx-auto w-full max-w-[1200px] border-b-4 border-white pb-3.5 font-semibold text-white placeholder:text-white/50 md:pb-10 md:text-center md:text-[48px] md:leading-[58px] md:-tracking-[1.44px]"
         />
 
         {!hasResults && (
-          <p className="body-large md:text-general-title mt-6 text-white/50 md:mt-10">
+          <p
+            className={cx('mt-2 text-white md:mt-7', {
+              'text-subheading': !isMobile,
+              'text-[10px] leading-3 font-semibold -tracking-[0.3px]': isMobile,
+            })}
+          >
             No results found for "{searchQuery}"
           </p>
         )}
